5105c56b4e109f60fbb3c3e0753df58a15249b1a
Evandro oliveira <vandoaparecido@hotmail.com>
2022-11-14 20:11:36 -0300

    :fork_and_knife: :fries: :pizza: Fazendo a conexão com o mongoDb

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+// mongoose.connect('mongodb://localhost:27017')
+// .then(() => console.log("Conectado ao mongo"))
+// .catch(() => console.log("Erro ao conectar ao mongoDb"))
 
-app.listen(3001, () => {
-  console.log('Server is running on https://localhost:3001')
+mongoose.connect('mongodb://localhost:27017')
+.then(() => {
+  const app = express();
+  app.listen(3001, () => {
+    console.log('Server is running on https://localhost:3001')
+  })
 })
+.catch(() => console.log("Erro ao conectar ao mongoDb"))
+
