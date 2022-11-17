8ca03f9020313bee620642af7353bdca8c08f90e
Evandro oliveira <vandoaparecido@hotmail.com>
2022-11-17 20:47:53 -0300

    :fork_and_knife: :fries: :pizza: Separando styled

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,18 +1,18 @@
-import styled from 'styled-components';
-import logo from '../assets/images/logo.svg';
+import {Container, Content} from './styles'
+import logo from '../../assets/images/logo.svg';
 
-const Container = styled.header`
-background: #D73035;
-`;
 
 export function Header(){
   return (
-    <Container>
+     <Container>
+      <Content>
         <div className="page-details">
           <h1>Pedidos</h1>
           <h2>Acompanhe os pedidos dos clientes</h2>
         </div>
+
         <img src={logo} alt="WAITERAPP" />
+      </Content>
     </Container>
   )
 }
