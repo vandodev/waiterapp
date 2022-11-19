fcd6fc8f35d4bac08e5610fc2f24e2bfab3244c3
Evandro oliveira <vandoaparecido@hotmail.com>
2022-11-19 02:08:13 -0300

    :fork_and_knife: :fries: :pizza: Mobile - Estrutura da página peincipal

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import {useFonts} from 'expo-font'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Main } from './src/Main';
 
 export default function App() {
 
@@ -15,18 +14,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{fontFamily:'GeneralSans-400'}}>Olá mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/Main/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin-top:56px ;
+  background: #fafafa;
+  `;
